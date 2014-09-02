b77b35816dae60fa49710efb6d71ae17514ee1bb
shamim-ahmed <shamim.buet.99@gmail.com>
2014-09-02 14:58:42 +1000

    now %20 character is replaced by a space in the tweet

---
+++ twitter-update-status/src/main/java/edu/buet/cse/mule/StringUtils.java
@@ -0,0 +1,11 @@
+package edu.buet.cse.mule;
+
+public class StringUtils {
+	public static String replaceEncodedSpaceChar(String source) {
+		if (source == null) {
+			return "";
+		}
+		
+		return source.replaceAll("%20", " ");
+	}
+}
