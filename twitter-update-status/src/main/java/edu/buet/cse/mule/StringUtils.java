0c6b5f0b7c0dbd26706ba6959526884e555d31bf
shamim-ahmed <shamim.buet.99@gmail.com>
2014-09-02 15:09:16 +1000

    formatted the java class

--- twitter-update-status/src/main/java/edu/buet/cse/mule/StringUtils.java
+++ twitter-update-status/src/main/java/edu/buet/cse/mule/StringUtils.java
@@ -1,11 +1,11 @@
 package edu.buet.cse.mule;
 
 public class StringUtils {
-	public static String replaceEncodedSpaceChar(String source) {
-		if (source == null) {
-			return "";
-		}
-		
-		return source.replaceAll("%20", " ");
-	}
+  public static String replaceEncodedSpaceChar(String source) {
+    if (source == null) {
+      return "";
+    }
+
+    return source.replaceAll("%20", " ");
+  }
 }
